cc0062e2ddc04ab92dc8d3b976a6b7d26c6b3bfe
Yaohui Wang <wangyaohuicn@gmail.com>
2023-03-30 17:36:55 +0800

    refactor: use more descriptive names & move private methods to helper class

--- src/main/kotlin/org/yh/statistics/model/FileAggregateResult.kt
+++ src/main/kotlin/org/yh/statistics/model/FileAggregateResult.kt
@@ -10,57 +10,71 @@ data class FileAggregateResult(
     var totalInMillis: Long = 0,
     var lastClosedTs: Timestamp = 0
 ) {
+    var fileState: StatisticsAction? = null
+    var focusState: FocusState = FocusLost(0)
+    private val openCountsUpdater = OpenCountsUpdater()
+    private val totalTimeUpdater = TotalTimeUpdater()
+    private val lastClosedTsUpdater = LastClosedTsUpdater()
 
-    private var fileState: StatisticsAction? = null
-    private var focusState: FocusState = FocusLost(0)
-
-    private fun updateOpenCounts(event: StatisticsEvent) {
-        val prevFileState = fileState
-        if (event.action in setOf(FILE_OPENED, FILE_CLOSED)) {
-            fileState = event.action
-        }
-        if (prevFileState == FILE_OPENED && fileState == FILE_CLOSED) {
-            openCounts++
-        }
-    }
-
-    private fun updateTotalTime(event: StatisticsEvent) {
-        when (val prevFocusState = focusState) {
-            is FocusLost -> if (event.action == FILE_OPENED || (fileState == FILE_OPENED && event.action == IDE_ACTIVATED)) {
-                focusState = FocusGained(event.ts)
-            }
-            is FocusGained -> if (event.action in setOf(FILE_CLOSED, IDE_DEACTIVATED)) {
-                focusState = FocusLost(event.ts)
-                val duration = focusState.ts - prevFocusState.ts
-                // TODO: makes timespans max duration configurable
-                totalInMillis += minOf(duration, Duration.ofMinutes(30).toMillis())
-            }
-        }
-    }
-
-    private fun updateLastClosedTs(event: StatisticsEvent) {
-        if (event.action == FILE_CLOSED) {
-            lastClosedTs = event.ts
-        }
-    }
-
+    /**
+     * Accumulates statistics for a single event
+     */
     fun accumulate(event: StatisticsEvent) {
-        updateOpenCounts(event)
-        updateTotalTime(event)
-        updateLastClosedTs(event)
+        openCountsUpdater.update(event, this)
+        totalTimeUpdater.update(event, this)
+        lastClosedTsUpdater.update(event, this)
     }
 
-    fun renderStatisticsHint() = template.render(this)
+    /**
+     * Renders a formatted string containing the statistics for this file
+     */
+    fun renderStatisticsHint() = formatter.format(this)
 }
 
-class StatisticsHintTemplate(private val template: String) {
+class OpenCountsUpdater {
+    fun update(event: StatisticsEvent, result: FileAggregateResult) {
+        val prevFileState = result.fileState
+        if (event.action in setOf(FILE_OPENED, FILE_CLOSED)) {
+            result.fileState = event.action
+        }
+        if (prevFileState == FILE_OPENED && result.fileState == FILE_CLOSED) {
+            result.openCounts++
+        }
+    }
+}
+
+class TotalTimeUpdater {
+    fun update(event: StatisticsEvent, result: FileAggregateResult) {
+        when (val prevFocusState = result.focusState) {
+            is FocusLost -> if (event.action == FILE_OPENED || (result.fileState == FILE_OPENED && event.action == IDE_ACTIVATED)) {
+                result.focusState = FocusGained(event.ts)
+            }
+            is FocusGained -> if (event.action in setOf(FILE_CLOSED, IDE_DEACTIVATED)) {
+                result.focusState = FocusLost(event.ts)
+                val duration = result.focusState.ts - prevFocusState.ts
+                // TODO: makes timespans max duration configurable
+                result.totalInMillis += minOf(duration, Duration.ofMinutes(30).toMillis())
+            }
+        }
+    }
+}
+
+class LastClosedTsUpdater {
+    fun update(event: StatisticsEvent, result: FileAggregateResult) {
+        if (event.action == FILE_CLOSED) {
+            result.lastClosedTs = event.ts
+        }
+    }
+}
+
+class FileStatisticsHintFormatter(private val template: String) {
 
     fun validate(): Boolean {
         // TODO: to be used in configuration panel
         return true
     }
 
-    fun render(result: FileAggregateResult): String {
+    fun format(result: FileAggregateResult): String {
         var text = template
         val (openCounts, totalInMillis, lastClosedTs) = result
         text = text.replace(LAST_VIEWED.pattern, if (lastClosedTs > 0) lastClosedTs.localDate?.delta.orEmpty() else "")
@@ -70,7 +84,7 @@ class StatisticsHintTemplate(private val template: String) {
     }
 }
 
-val template = StatisticsHintTemplate(System.getProperty("statisticsView.hint.template", DEFAULT_HINT_TEMPLATE))
+val formatter = FileStatisticsHintFormatter(System.getProperty("statisticsView.hint.template", DEFAULT_HINT_TEMPLATE))
 
 sealed class FocusState(var ts: Timestamp)
 class FocusGained(ts: Timestamp) : FocusState(ts)
